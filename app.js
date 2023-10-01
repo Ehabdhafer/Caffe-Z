4669050c84865b0ae5e198f696df53950aefe7be
Ehabdhafer <ehabzaferabed@gmail.com>
2023-10-01 18:06:57 +0300

    add loop and function

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-var name = prompt("What is your Name");
-var gender = prompt("Your Gender");
+var gender;
+function getgender() {
+  while (gender != "male" && gender != "female") {
+    gender = prompt("Your Gender");
+  }
 
-if (gender == "male") {
-  alert("Welcome Mr" + " " + name);
-} else if (gender == "female") {
-  alert("Welcome Ms" + " " + name);
-} else {
-  alert("Welcome" + " " + name);
+  if (gender == "male") {
+    alert("Welcome Mr" + " " + name);
+  } else if (gender == "female") {
+    alert("Welcome Ms" + " " + name);
+  }
 }
-
+var name = prompt("What is your Name");
+var title = getgender();
 var drink = prompt("Do you want to drink a hot or cold drink ?");
 
-if (drink == "yes") {
+if (drink == "hot" || drink == "cold") {
   var dname = prompt("write the name of the drink you want");
 
   alert("Your drink is getting prepared");
@@ -20,4 +23,10 @@ if (drink == "yes") {
 } else {
   alert("You not ordered anything");
   console.log("no orders");
+}
+
+let array = [name, gender, drink, dname];
+
+for (i = 0; i < array.length; i++) {
+  console.log(array[i]);
 }
